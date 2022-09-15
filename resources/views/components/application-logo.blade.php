6bb9e7641ac4ed90792e4cbb09f898a5e00edf3d
Pooja SHARMA <19946362@student.westernsydney.edu.au>
2022-09-15 12:47:13 +1000

    First commit

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="https://lh5.googleusercontent.com/NVaH7v-8XCz7MsRXyKuJy1oU7WpCaPipXnDo_rHrPOJ6OLZ0C0sJrovNaVOqFfSyULHoj2GUVBtKWHxaDmY3XlJzD_apk-O28cH0Y8k43PDQmQ4AQ2mq0YxLC3TbqlU4TPruW4xF7TScLd6kI5WuFg=w234-h69" style="width:inherit;height:inherit;object-fit:scale-down;object-position:left bottom;" />
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
+use App\Models\User;
+use App\Http\Controllers\AuthenticatedSessionController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+// Admin Route
+
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+    Route::namespace('Auth')->middleware('guest:admin')->group(function(){
+        // login route
+        Route::get('login', 'AuthenticatedSessionController@create')->name('login');
+        Route::post('login', 'AuthenticatedSessionController@store')->name('adminlogin');
+    });
+    Route::middleware('admin')->group(function(){
+        Route::get('dashboard', 'HomeController@index')->name('dashboard');
+    });
+    Route::post('logout', 'Auth\AuthenticatedSessionController@destroy')->name('logout');
+});
+
